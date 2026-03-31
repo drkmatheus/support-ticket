f5c2729f42d23ce501f3e04fe084141ac35cf29d
drkmatheus <matheus.gcrosciati@gmail.com>
2026-03-31 09:21:00 -0300

    [drkmatheus] Middleware de "serialização json" e teste com request client

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import http from "node:http";
 
-function listener(req, res) {
-  return res.end("Working!");
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+async function listener(req, res) {
+  await jsonHandler(req, res);
+  console.log(req.body);
+
+  return res.end(JSON.stringify("Resposta enviada!"));
 }
 
 http.createServer(listener).listen(3333);
